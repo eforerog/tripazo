2ac4c6df900af38fa83ed03c100c96e38aed6581
Edgar Forero <Edgar@MacBook.local>
2013-03-31 23:49:27 -0500

    tripazo

--- static/js/function.js
+++ static/js/function.js
@@ -1,5 +1,55 @@
 // JavaScript Document
 /*traveler type*/
+var expand_act = 0;
+var expand_state = Array();
+
+function init(day_max) {
+	for(var i = 0; i <= day_max; i++)
+	{
+		expand_state.push(0);
+	}
+}
+
+function expand_detail(id, day_n, day_max) {
+	//alert(day_max);
+	for(var i = 1; i <= day_max +1; i++)
+	{
+		if(i == day_n)
+		{
+			if(expand_state[i-1] == 0)
+			{
+				expand_state[i-1] = 1;
+				document.getElementById("expansion_pre_" + i).style.height = "400px";
+			}
+			else
+			{
+				expand_state[i-1] = 0;
+				document.getElementById("expansion_pre_" + i).style.height = "0px";
+			}
+		}
+		else
+		{
+			document.getElementById("expansion_pre_" + i).style.height = "0px";
+			expand_state[i-1] = 0;
+		}
+	
+	
+		
+	}
+	
+	if(expand_state[day_n-1] == 0)
+	{
+		//expand_state[day_n-1] = 1;
+		//document.getElementById("expansion_pre_" + day_n).style.height = "400px";
+	}
+	else
+	{
+		//expand_state[day_n-1] = 0;
+		//document.getElementById("expansion_pre_" + day_n).style.height = "0px";
+	}
+	
+}
+
  $(document).ready(function() {
 
             $(".drop").click(function(e) {          
@@ -67,9 +117,9 @@ $(document).ready(function() {
 $(document).ready(function() {
 
             $(".deploy").click(function(e) {          
-				e.preventDefault();
-                $("div#options").toggle();
-				$(".deploy").toggleClass("up");
+				//e.preventDefault();
+                //$("div#options").toggle();
+				//$(".deploy").toggleClass("up");
             });
 			
 			$("div#options").mouseup(function() {
@@ -77,8 +127,8 @@ $(document).ready(function() {
 			});
 			$(document).mouseup(function(e) {
 				if($(e.target).parent("a.deploy").length==0) {
-					$(".deploy").removeClass("up");
-					$("div#options").hide();
+					//$(".deploy").removeClass("up");
+					//$("div#options").hide();
 				}
 			});			
 			
@@ -100,8 +150,8 @@ $(document).ready(function() {
 	
 /*jquery calendar calling*/
 $(function() {
-$( "#datepicker" ).datepicker();
-$( "#datepicker2" ).datepicker();
+$( "#datepicker" ).datepicker({ dateFormat: 'dd-mm-yy' });
+$( "#datepicker2" ).datepicker({ dateFormat: 'dd-mm-yy' });
 });
 
 
--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from array import array
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,6 +7,7 @@ from web.models import city
 from web.models import type_travel
 from web.models import place
 from web.models import budget
+from datetime import datetime
 
 def index(request):
     cities_list = city.objects.order_by('name')
@@ -16,14 +18,26 @@ def index(request):
     return render(request,'web/index.html',context)
     
 def itinerary(request):
+    month_list = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEPT', 'OCT','NOV', 'DEC']
     cities_list = city.objects.order_by('name')
     types_list = type_travel.objects.order_by('name')
     budget_list = budget.objects.order_by('name')
-    places_list = place.objects.order_by('name')
-    context = {'cities_list':cities_list,'places_list':places_list, 'budget_list':budget_list, 'types_list':types_list}
     if request.method == 'POST':
+        city_id = request.POST.get('city','')
+        from_date = datetime.strptime(request.POST.get('from',''),'%d-%m-%Y')
+        to_date = datetime.strptime(request.POST.get('to',''),'%d-%m-%Y')
+        date_sub = to_date - from_date
+        date_sub_days = range(1, date_sub.days+2)
+        date_sub_days_max = date_sub.days
+        month_from = month_list[from_date.month-1]
+        month_to = month_list[to_date.month-1]
+        type_travel_id = request.POST.getlist('type_travel')
+        
+        places_list = place.objects.order_by('name')[:date_sub_days_max*5]
+        context = {'type_travel_id': type_travel_id, 'cities_list':cities_list,'places_list':places_list, 'budget_list':budget_list, 'types_list':types_list, 'date_sub_days':date_sub_days, 'from_date': from_date, 'to_date':to_date, 'month_from':month_from, 'month_to':month_to, 'date_sub_days_max': date_sub_days_max}
         return render(request, 'web/itinerary.html', context)
     else:
+        context = {'cities_list':cities_list,'places_list':places_list, 'budget_list':budget_list, 'types_list':types_list}
         return render(request,'web/index.html',context)
         
    
